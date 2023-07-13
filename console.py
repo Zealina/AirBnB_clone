dca2270791d970338da3065ac4743b3d32cbbff2
Hamilton Sebastine <hamilsebastine@gmail.com>
2023-07-13 02:34:59 +0000

    Console Complete

--- console.py
+++ console.py
@@ -53,6 +53,45 @@ class HBNBCommand(cmd.Cmd):
             storage.save()
         except KeyError:
             print("** no instance found **")
+    
+    def do_all(self, arg):
+        """
+        Prints string representation of all instances of all classes
+        """
+        all_inst = storage.all()
+        if arg and arg not in self.__classes:
+            print("** class doesn't exist **")
+        else:
+            print([str(inst) for inst in all_inst.values()])
+
+    def do_update(self, arg):
+        """
+        Updates an instance based on the class name and id by adding or 
+        updating attribute
+        """
+        arg = self.parser(arg)
+        if not arg:
+            return
+        all_inst = storage.all()
+        key = arg[0] + '.' + arg[1]
+        if key not in all_inst:
+            print("** no instance found **")
+        elif len(arg) < 3:
+            print("** attribute name missing **")
+        elif len(arg) < 4:
+            print("** value missing **")
+        else:
+            data_types = [int, float, str]
+            for d_type in data_types:
+                try:
+                    if d_type is str:
+                        arg[3] = arg[3][1:-1]
+                    value = d_type(arg[3])
+                    all_inst[key].__setattr__(arg[2], value)
+                    break
+                except ValueError:
+                    continue
+
 
     def emptyline(self):
         """Called when an emptyline is entered

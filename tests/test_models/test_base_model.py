22df266f68b19819b10a9664f9701a360ac069e5
Seidu Mohammed <seidumohammed64@gmail.com>
2023-07-15 21:07:03 +0000

    refactored the test files for user, base_model and file_storage

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -12,46 +12,41 @@ class TestBaseModel(TestCase):
     """A class for various test cases for the BaseModel
     """
 
-    def setUp(self) -> None:
-        return super().setUp()
+    def setUp(self):
+        self.b_model = BaseModel()
+        self.b_model2 = BaseModel()
 
     def test_attr_id_instance(self):
         """Test the attr `id` for the correct instance
         """
-        b_model = BaseModel()
-        self.assertIsInstance(b_model.id, str)
+        self.assertIsInstance(self.b_model.id, str)
 
     def test_attr_created_at_instance(self):
         """Test the instance attr `created_at`
         """
-        b_model = BaseModel()
-        self.assertIsInstance(b_model.created_at, datetime)
+        self.assertIsInstance(self.b_model.created_at, datetime)
 
     def test_attr_updated_at_instance(self):
         """Test the instance attr `updated_at`
         """
-        b_model = BaseModel()
-        self.assertIsInstance(b_model.updated_at, datetime)
+        self.assertIsInstance(self.b_model.updated_at, datetime)
 
     def test_base_model_save_method(self):
         """Test the save method in base_model
         """
-        b_model = BaseModel()
-        b_model.save()
-        self.assertIsInstance(b_model.updated_at, datetime)
+        self.b_model.save()
+        self.assertIsInstance(self.b_model.updated_at, datetime)
 
     def test_base_model_to_dict_method(self):
         """Test to_dict method in base_model
         """
-        b_model = BaseModel()
-        b_model.to_dict()
-        self.assertIsInstance(b_model.to_dict(), dict)
+        self.b_model.to_dict()
+        self.assertIsInstance(self.b_model.to_dict(), dict)
 
     def test_base_model_to_dict_correct_type(self):
         """Test for the correct type of to_dict method
         """
-        b_model = BaseModel()
-        content = b_model.to_dict()
+        content = self.b_model.to_dict()
         self.assertEqual(type(content['created_at']), str)
         self.assertEqual(type(content['updated_at']), str)
         self.assertEqual(type(content['id']), str)
@@ -60,22 +55,20 @@ class TestBaseModel(TestCase):
     def test_create_base_model_from_dict(self):
         """Test the create BaseModel from dictionary
         """
-        b_model = BaseModel()
-        content = b_model.to_dict()
+        content = self.b_model.to_dict()
         b_model2 = BaseModel(**content)
-        self.assertEqual(b_model2.id, b_model.id)
+        self.assertEqual(b_model2.id, self.b_model.id)
         self.assertEqual(type(b_model2.created_at), datetime)
         self.assertEqual(type(b_model2.updated_at), datetime)
-        self.assertEqual(b_model2.created_at, b_model.created_at)
-        self.assertEqual(b_model2.updated_at, b_model.updated_at)
+        self.assertEqual(b_model2.created_at, self.b_model.created_at)
+        self.assertEqual(b_model2.updated_at, self.b_model.updated_at)
 
     def test_two_equal_object(self):
         """Teste if a new object created is same as first object
         """
-        b_model = BaseModel()
-        content = b_model.to_dict()
+        content = self.b_model.to_dict()
         b_model2 = BaseModel(**content)
-        self.assertIsNot(b_model, b_model2)
+        self.assertIsNot(self.b_model, b_model2)
 
 
 if __name__ == "__main__":
